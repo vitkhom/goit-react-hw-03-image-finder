02d70986528040e5991ddf189b5ecc0447708f9b
vitkhom <vitalii.s.khomenko@gmail.com>
2020-06-30 22:59:42 +0300

    Folder common added

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
-import Loader from './Loader';
-import Button from './Button';
-import Modal from './Modal';
+import Loader from '../common/Loader';
+import Button from '../common/Button';
+import Modal from '../common/Modal';
 
 import photosApi from '../utils/photosApi';
 
